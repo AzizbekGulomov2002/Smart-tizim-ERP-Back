5bd2b6d1f0a5100a8900b6b5461c7b3ceaaa715f
AzizbekGulomov2002 <azizgulomov1529@gmail.com>
2024-04-03 21:47:27 +0300

    deploy

--- config/settings.py
+++ config/settings.py
@@ -236,7 +236,8 @@ JAZZMIN_SETTINGS = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
+STATIC_ROOT = "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
