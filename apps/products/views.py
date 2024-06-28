9ade43d32d8ef0e1de60f99865b6384c58486bff
Azizbek G`ulomov <84550212+AzizbekGulomov2002@users.noreply.github.com>
2024-06-28 14:20:44 +0300

    deploy

--- apps/products/views.py
+++ apps/products/views.py
@@ -7,7 +7,7 @@ from rest_framework.permissions import IsAuthenticated,AllowAny
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from apps.trade.views import CustomPaginationMixin, BasePagination
-from apps.product.filters import *
+from apps.products.filters import *
 from apps.app.models import StorageProductOff
 from .serializers import  *
 from .decorator import is_storage_permission, is_product_permission
